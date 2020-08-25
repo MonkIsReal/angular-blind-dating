33c902950fc8682011b637523288d737d903063b
borei <borei.eng@gmail.com>
2020-08-26 01:16:34 +0200

    commit results description
    some wording mistakes

--- src/app/surveyresult/surveyresult.component.ts
+++ src/app/surveyresult/surveyresult.component.ts
@@ -18,17 +18,17 @@ export class SurveyresultComponent implements OnInit {
   profiles: result[] = [
     {
       name: "Simp",
-      description:"sorry you have the worse profile of all. you need to put women out of their pedestal. some rules should be applies like never pay women for free. in your case you should massive approach woman and be sexual",
+      description:"Sorry but you have the worse profile of all. You need to put women out of their pedestals. Some rules should be applied to you, like never pay women for free. In your case you should do massive approaches on woman and be more sexual.",
       url:"assets/simp.png"
     },
     {
       name: "Nice guy",
-      description: "the nice guy is often friend with women but never settle down. when women says LJBF, she's never mean it. you should understand that she means you are not authentic and you're flat. advice: tell her that she's wrong when you think that sh's wrong",
+      description: "The nice guy is often friend with women but he never settle down with them. When women says LJBF, she's never meant it. You should understand that she meant that you are not authentic and you're flat. Advice: tell her that she's wrong when necessery and have more boundaries.",
       url:"assets/niceguy.jpg"
     },
     {
       name: "Apha",
-      description: "Handsome boy.., guess that your sexual life is easy and you had a lot of partner. unfortunately you have a great chance to loose the ability to pair bond and settle down with a women. if you want to have children you should fix your sexual addiction, and stop to flirt with women that does not want to settle down",
+      description: "Handsome boy.., guess that your sexual life is easy and you had a lot of partners. Unfortunately you have a great chance to loose the ability to pair bond and settle down with a women. If you want to have children you should fix your sexual addiction, and stop to flirting with random women that does not want to settle down.",
       url:"assets/handsome.jpg"
     },
   ];
